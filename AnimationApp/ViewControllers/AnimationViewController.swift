ad7a559a1841cfef38aaa427d9ef9606b3f129c1
CemenVy <semen.vydrin0108@gmail.com>
2023-09-05 18:51:31 +0400

    Added outlets and Action in AnimationViewController

---
+++ AnimationApp/ViewControllers/AnimationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by Семен Выдрин on 05.09.2023.
+//
+
+import UIKit
+
+class AnimationViewController: UIViewController {
+
+    @IBOutlet var presentLabel: UILabel!
+    @IBOutlet var curveLabel: UILabel!
+    @IBOutlet var forceLabel: UILabel!
+    @IBOutlet var durationLabel: UILabel!
+    @IBOutlet var delayLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func runSpringAnimationDidTapped(_ sender: UIButton) {
+    }
+    
+}
+
