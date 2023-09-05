618174c7d4c683e0d1bdb6413091278761af6219
CemenVy <semen.vydrin0108@gmail.com>
2023-09-05 11:48:28 +0400

    Added new directories

---
+++ AnimationApp/Models/Animation.swift
@@ -0,0 +1,7 @@
+//
+//  Animation.swift
+//  AnimationApp
+//
+//  Created by Семен Выдрин on 05.09.2023.
+//
+
---
+++ AnimationApp/Services/DataStore.swift
@@ -0,0 +1,8 @@
+//
+//  DataStore.swift
+//  AnimationApp
+//
+//  Created by Семен Выдрин on 05.09.2023.
+//
+
+import Foundation
