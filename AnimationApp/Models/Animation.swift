cb6f5080fb5fc37d7c50f6a717b3e720cadea689
CemenVy <semen.vydrin0108@gmail.com>
2023-09-05 12:29:47 +0400

    Added data in model Animation

--- AnimationApp/Models/Animation.swift
+++ AnimationApp/Models/Animation.swift
@@ -5,3 +5,13 @@
 //  Created by Семен Выдрин on 05.09.2023.
 //
 
+struct Animation {
+    var title: String
+    var curve: String
+    
+    var force: Double
+    var duration: Double
+    var delay: Double
+    
+    static func getAnimation() {}
+}
